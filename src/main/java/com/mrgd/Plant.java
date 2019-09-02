b56b93269a68f6f40a13bb6b050850d1724de809
aaaaalei123 <745514499@qq.com>
2019-09-02 15:36:57 +0800

    添加鼠标悬停操作

--- src/main/java/com/mrgd/Plant.java
+++ src/main/java/com/mrgd/Plant.java
@@ -13,7 +13,7 @@ public class Plant {
 	    webDriver.findElement(By.xpath("/html/body/div/div[2]/ul/li[3]")).click();
 	    Thread.sleep(1500);
 	    // 搜索品种
-/*	    webDriver.findElement(By.xpath("/html/body/div/div[3]/div/div/div/input")).sendKeys("脐橙");
+	    webDriver.findElement(By.xpath("/html/body/div/div[3]/div/div/div/input")).sendKeys("脐橙");
 	    Thread.sleep(1000);
 	    webDriver.findElement(By.cssSelector("div[data-v-1e9350d5][class='button']")).click();
 	    Thread.sleep(1500);
@@ -30,9 +30,9 @@ public class Plant {
 	    //点击江西省
 		actions.moveToElement(webDriver.findElement(By.xpath("//*[@id='app']")), 650, 700).click().perform();
 		Thread.sleep(1000);
-	    */
+	    
 		// 农作物树龄分布
-/*	    actions.moveToElement(webDriver.findElement(By.xpath("//*[@id='app']/div[3]/div[2]/div[2]/div[2]/div[1]/div/div[1]")), 100, 33).perform();
+	    actions.moveToElement(webDriver.findElement(By.xpath("//*[@id='app']/div[3]/div[2]/div[2]/div[2]/div[1]/div/div[1]")), 100, 33).perform();
 		Thread.sleep(1000);
 		actions.moveToElement(webDriver.findElement(By.xpath("//*[@id='app']/div[3]/div[2]/div[2]/div[2]/div[1]/div/div[1]")), 100, 54).perform();
 		Thread.sleep(1000);
@@ -57,7 +57,7 @@ public class Plant {
 		actions.moveToElement(webDriver.findElement(By.xpath("//*[@id='app']/div[3]/div[3]/div[1]/div[2]/div[1]/div[2]/div[1]")), 100, 64).perform();
 		Thread.sleep(1000);
 		actions.moveToElement(webDriver.findElement(By.xpath("//*[@id='app']/div[3]/div[3]/div[1]/div[2]/div[1]/div[2]/div[1]")), 100, 116).perform();
-		Thread.sleep(1000);*/
+		Thread.sleep(1000);
 		
 		// 产品品质分布
 		actions.moveToElement(webDriver.findElement(By.xpath("//*[@id='app']/div[3]/div[3]/div[2]/div[2]/div[1]/div/div[1]")), 50, 80).perform();
